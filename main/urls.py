0b5cf8cc45f7761e4a9191425c3f9384c32ba4ce
nicomiguelino <nicomiguelino2014@gmail.com>
2020-06-10 20:34:00 +0800

    Added view and template for the first Form class example

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import IndexView
+from .views import (
+    IndexView,
+    Example01
+)
 
 app_name = 'main'
 urlpatterns = [
-    path('', IndexView.as_view(), name='index')
+    path('', IndexView.as_view(), name='index'),
+    path('example_01/', Example01.as_view(), name='example_01')
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.views import View
+from .forms import ContactForm
 
 class IndexView(View):
     def get(self, request):
         return render(request, 'main/index.html')
+
+class Example01(View):
+    """Using the Form class to render HTML forms in templates."""
+    def get(self, request):
+        contact_form = ContactForm()
+
+        return render(request, 'main/example_01.html', {
+            'contact_form': contact_form
+        })
